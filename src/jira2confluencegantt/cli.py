cdad423cea47e509ee1829d49faa344ebcdfd796
Deanna Church <deanna.church@incripta.com>
2022-05-09 11:57:01 -0600

    fix typo

--- src/jira2confluencegantt/cli.py
+++ src/jira2confluencegantt/cli.py
@@ -15,9 +15,9 @@ from .report import generate_all_reports
 
 class LoadFromFile (argparse.Action):
     def __call__ (self, parser, namespace, values, option_string=None):
-    with values as f:
-        # parse arguments in the file and store them in the target namespace
-        parser.parse_args(f.read().split(), namespace)
+        with values as f:
+            # parse arguments in the file and store them in the target namespace
+            parser.parse_args(f.read().split(), namespace)
 
 def _input_password() -> str:
     """
